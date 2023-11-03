e233153cfc0fda4846b14f53a74e9eb3b4af68ac
wndud5814 <wndud5814@naver.com>
2023-11-03 14:17:41 +0900

    clock

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert("Hello, world");
+const clock = document.querySelector(".clock");
+
+function getTime() {
+  const date = new Date();
+
+  let amPm = "AM";
+
+  // let hours = String(date.getHours()).padStart(2, "0");
+  let hours = date.getHours();
+
+  if (hours >= 12 && hours !== 24) amPm = "PM";
+
+  if (hours >= 13) {
+    hours %= 12;
+    // hours = hours % 12;
+  }
+
+  // let hours2 = date.getHours() >= 13 ? date.getHours() : date.getHours() % 12;
+
+  hours = String(hours).padStart(2, "0");
+
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  let seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${amPm} ${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
