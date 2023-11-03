f276fbc201cc51e169489ec2b574e0c758aff9de
wndud5814 <wndud5814@naver.com>
2023-11-03 17:59:55 +0900

    nftpage

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ function getTime() {
   // let hours = String(date.getHours()).padStart(2, "0");
   let hours = date.getHours();
 
-  if (hours >= 12 && hours !== 24) amPm = "PM";
+  if (hours >= 12) amPm = "PM";
 
   if (hours >= 13) {
     hours %= 12;
     // hours = hours % 12;
   }
 
-  // let hours2 = date.getHours() >= 13 ? date.getHours() : date.getHours() % 12;
+  // let hours2 = date.getHours() >= 13 ? date.getHours() % 12 : date.getHours();
 
   hours = String(hours).padStart(2, "0");
 
@@ -27,3 +27,55 @@ function getTime() {
 
 getTime();
 setInterval(getTime, 1000);
+
+const QUOTE_LIST = "quotesList";
+
+function getQuotes() {
+  const quotes = document.querySelector(".quotes");
+
+  let savedQuotes = localStorage.getItem(QUOTE_LIST);
+
+  if (!savedQuotes) {
+    localStorage.setItem(QUOTE_LIST, JSON.stringify(["열심히 살자!"]));
+
+    savedQuotes = localStorage.getItem(QUOTE_LIST);
+  }
+
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  quotes.innerText =
+    parsedQuotes[Math.floor(Math.random() * parsedQuotes.length)];
+}
+
+getQuotes();
+
+function onClickNewQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuotes = document.querySelector(".new-quotes");
+  const newQuotesInput = document.querySelector(".new-quotes-input");
+
+  if (!newQuotesInput.value) return;
+
+  // 로컬 스토리지에 저장
+  let savedQuotes = localStorage.getItem(QUOTES_LIST);
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  parsedQuotes.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES_LIST, JSON.stringify(parsedQuotes));
+
+  // 현재 페이지 반영
+  quotes.innerText = newQuotesInput.value;
+  newQuotesInput.value = "";
+
+  quotes.style.display = "block";
+  newQuotes.style.display = "none";
+}
+
+function onClickQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuotes = document.querySelector(".new-quotes");
+
+  quotes.style.display = "none";
+  newQuotes.style.display = "block";
+}
